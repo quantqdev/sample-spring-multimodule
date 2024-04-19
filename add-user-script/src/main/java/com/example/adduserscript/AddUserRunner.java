4a028344ace8ad7d77ff2992863a152085756431
Quan Ta <quantq.dev@gmail.com>
2024-04-19 16:51:11 +0700

    Refactor application properties

--- add-user-script/src/main/java/com/example/adduserscript/AddUserRunner.java
+++ add-user-script/src/main/java/com/example/adduserscript/AddUserRunner.java
@@ -4,6 +4,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.example.lib.configuration.MyConfig;
 import com.example.lib.service.PostService;
 
 import lombok.NonNull;
@@ -14,9 +15,12 @@ import lombok.RequiredArgsConstructor;
 public class AddUserRunner implements ApplicationRunner {
 
     private final PostService postService;
+    private final MyConfig myConfig;
 
     @Override
     public void run(@NonNull final ApplicationArguments args) throws Exception {
+        System.out.println("Greeting message: " + myConfig.greetingMessage());
+
         final var savedPost = postService.createPost("post aus", "post created by add-user-runner");
         System.out.println(savedPost);
     }
